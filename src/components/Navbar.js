95cc570a6314bee74809930f227d63b2e476ff00
shawn <shawn.dagisi@student.moringaschool.com>
2023-11-23 16:25:22 +0300

    Navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,44 +7,17 @@ const Navbar = () => {
       <ul>
         <li>
           <NavLink to="/" exact activeClassName="active">
-          Player
+          Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/about" activeClassName="active">
-            About
+          <NavLink to="/playlist" activeClassName="active">
+            Playlist
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Top songs" activeClassName="active">
-            charts
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-};
-
-export default Navbar;import React from 'react';
-import { NavLink } from 'react-router-dom';
-
-const Navbar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" activeClassName="active">
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact" activeClassName="active">
-            Contact
+          <NavLink to="/charts" activeClassName="active">
+            Charts
           </NavLink>
         </li>
       </ul>
